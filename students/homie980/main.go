14f4d494ff5578885ca3267e433a092d6b65fc58
Abdelrahman Khaled <boodykhaled@gmail.com>
2021-07-22 20:51:15 +0200

    added YAMLHandler

--- students/homie980/main.go
+++ students/homie980/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/gophercises/urlshort"
+	"github.com/Homie980/go-urlshortener/students/homie980/urlshort"
 	"net/http"
 )
 
@@ -15,18 +15,18 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	//	yaml := `
-	//- path: /urlshort
-	//  url: https://github.com/gophercises/urlshort
-	//- path: /urlshort-final
-	//  url: https://github.com/gophercises/urlshort/tree/solution
-	//`
-	//	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	//	if err != nil {
-	//		panic(err)
-	//	}
+	yaml := `
+	- path: /urlshort
+	 url: https://github.com/gophercises/urlshort
+	- path: /urlshort-final
+	 url: https://github.com/gophercises/urlshort/tree/solution
+	`
+	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", mapHandler)
+	http.ListenAndServe(":8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
--- students/homie980/urlshort/handler.go
+++ students/homie980/urlshort/handler.go
@@ -1,6 +1,9 @@
 package urlshort
 
-import "net/http"
+import (
+	"gopkg.in/yaml.v2"
+	"net/http"
+)
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +14,25 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 		}
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// Parse the yaml
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(yamlBytes, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	// convert yaml array into map
+	pathsToUrls := make(map[string]string{})
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	// return a map handler using the map
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
